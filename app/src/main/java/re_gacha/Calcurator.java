a44995c570a9d18b48e183b819c94d50c39856b2
20j <20j.code@gmail.com>
2023-06-09 19:24:11 +0900

    feat : make Calcurator
    테스트 코드를 짜는데도 시간이 걸리는게 무슨말인지 알것같다.

--- app/src/main/java/re_gacha/Calcurator.java
+++ app/src/main/java/re_gacha/Calcurator.java
@@ -1,7 +1,24 @@
 package re_gacha;
+import java.util.Random;
+
 
 public class Calcurator {
     public int gachaOnceNumber(double num) {
-        return 12;
+        Random random = new Random();
+
+        boolean isSucess = false;
+
+        int counter = 0;
+
+        while(!isSucess) {
+            counter++;
+            double ans = random.nextDouble();
+
+            if (ans> 1-num) {
+                isSucess = true;
+            }
+        }
+
+        return counter;
     }
 }
