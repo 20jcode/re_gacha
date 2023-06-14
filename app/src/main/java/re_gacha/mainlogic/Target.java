bc4c37d31defc7b07779ee584469a4f8a4f3b897
20j <20j.code@gmail.com>
2023-06-14 14:42:50 +0900

    feat : Add Target

--- app/src/main/java/re_gacha/mainlogic/Target.java
+++ app/src/main/java/re_gacha/mainlogic/Target.java
@@ -1,5 +1,11 @@
 package re_gacha.mainlogic;
 
 public class Target {
-    
+    public void makeTarget(Object item,Object gachainfo) {
+
+    }
+
+    public String getTarget(Item item) {
+        
+    }
 }
