9ea41a49db6e462ede9c1a8474995593676ac31d
20j <20j.code@gmail.com>
2023-06-14 13:26:59 +0900

    feat : Add item
    Item.java 공용 인터페이스 작성

--- app/src/main/java/re_gacha/mainlogic/Item.java
+++ app/src/main/java/re_gacha/mainlogic/Item.java
@@ -1,5 +1,12 @@
 package re_gacha.mainlogic;
 
 public class Item {
-    
+    public Item makeItem(Object iteminfo) {
+        
+    }
+
+    public Item changeItem(int itemvalue) {
+        
+    }
+
 }
