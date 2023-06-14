d33023abb6683a7721c725fb28762dace134b7f4
20j <20j.code@gmail.com>
2023-06-14 14:46:00 +0900

    feat : Add gacha

--- app/src/main/java/re_gacha/mainlogic/Gacha.java
+++ app/src/main/java/re_gacha/mainlogic/Gacha.java
@@ -1,5 +1,11 @@
 package re_gacha.mainlogic;
 
 public class Gacha {
-    
+    public void makeGacha(Item item,Object gachainfo) {
+
+    }
+
+    public Object doGacha(Item item,Object gachavalue) {
+        
+    }
 }
