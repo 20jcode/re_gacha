f3d0cc40f4729c9cb037b3922c3f2a4287005541
20j <20j.code@gmail.com>
2023-06-14 11:53:22 +0900

    feat : Add class file
    인터페이스 구상을 위한 파일 설계
    User.java 인터페이스 생성

---
+++ app/src/main/java/re_gacha/mainlogic/User.java
@@ -0,0 +1,10 @@
+package re_gacha.mainlogic;
+
+public class User {
+    public void makeGacha(Object gachainfo, Object iteminfo) {
+
+    }
+    public void calItemValue(Object targetinfo, Object itemvalue) {
+        
+    }
+}
