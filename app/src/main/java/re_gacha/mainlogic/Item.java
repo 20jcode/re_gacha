14e3a5b85a380ff941c340871515ed0c96bf617e
20j <20j.code@gmail.com>
2023-06-22 09:16:42 +0900

    test : Item method
    Item 메소드 구현중

--- app/src/main/java/re_gacha/mainlogic/Item.java
+++ app/src/main/java/re_gacha/mainlogic/Item.java
@@ -4,15 +4,20 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class Item {
-    public void AddItem(String item, int value) {
+    
+    public void createItem(Map<String,Integer> obj) {
+
+    }
+    
+    public void addItem(String item, int value) { //새로운 아이템 추가
         itemMap.put(item,value);
     }
 
-    public void changeItem(String item, int itemvalue) {
+    public void decItem(String item, int itemvalue) {
 
     }
 
-    public int getItem(String item) {
+    public int getItem(String item) { //현재 아이템의 남은 수량 확인
         return itemMap.get(item);
     }
 
--- app/src/test/java/re_gacha/ItemTest.java
+++ app/src/test/java/re_gacha/ItemTest.java
@@ -5,30 +5,33 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import re_gacha.mainlogic.Item;
 
+import java.util.Map;
+import java.util.HashMap;
+
+
 public class ItemTest {
     Item item = new Item();
 
     @Test //정상적으로 아이템이 추가되는가?
-    void AddItemTest(){
+    void addItemTest(){
         String itemname = "블랙티켓";
         int value = 10;
 
-        item.AddItem(itemname,value);
+        item.addItem(itemname,value);
 
         assertNotNull(item,"not null");
         
     }
-    @Test
-    void GetItemTest(){
-        String itemname = "블랙티켓";
-        int value = 10;
+    @Test // createItem() 에 대해서
+    void createItemTest(){
+        Map<String, Integer> itemMaptest = new HashMap<>();
+        
+        // test데이터 입력
+        itemMaptest.put("블랙티켓",3);
+        itemMaptest.put("경비",12);
 
-        item.AddItem(itemname,value);
-
-
-        int result = item.getItem(itemname);
-
-        assertEquals(result,10);
+        item.createItem(itemMaptest);
+        
     }
 
 }
